2417920e15b557db2cee05d68ade680f662d4c28
wjllp <1242924472@qq.com>
2018-05-08 19:58:20 +0800

    增加注释

--- douyin.py
+++ douyin.py
@@ -2,20 +2,19 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-session = requests.session()
 url = ''
-filename = "urls.txt"
+filename = "urls.txt"  #用于保存解析出的url
 def download(userid,count):
-    url = 'https://www.douyin.com/aweme/v1/aweme/favorite/?user_id='+str(userid)+'&count='+str(count)+'&max_cursor=0&aid=1128'
-    resp = session.get(url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    myjson = json.loads(str(soup))
+    url = 'https://www.douyin.com/aweme/v1/aweme/favorite/?user_id='+str(userid)+'&count='+str(count)+'&max_cursor=0&aid=1128'   #请求url
+    resp = requests.get(url)  #get请求
+    soup = BeautifulSoup(resp.text, 'html.parser')  #解析html
+    myjson = json.loads(str(soup))     #转化为json格式数据
     with open(filename,"a+") as f:
         for i in range(count):
-            video_url = myjson['aweme_list'][i]['video']['play_addr']['url_list'][0]
+            video_url = myjson['aweme_list'][i]['video']['play_addr']['url_list'][0]   #提取url
             print (myjson['aweme_list'][i]['video']['play_addr']['url_list'][0])
-            f.write(video_url+"\n")
-    f.close()
+            f.write(video_url+"\n")     #写入文件
+    f.close()  #关闭文件
 
 if __name__ == '__main__':
-    download(94069666459,5)
+    download(94069666459,5)   #参数1：用户id，参数2：要下载的数量
